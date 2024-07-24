8825876f8042e3920f9f73bc6f8eb41708e0661a
DIMITRIDAR <daraseliadimitri@gmail.com>
2024-07-24 12:29:47 +0400

    Access to #people and #result-tip ID elements

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
 const billInput = document.getElementById("bill");
 const tipInputs = Array.from(document.getElementsByClassName("tip"));
+const peopleInput = document.getElementById("people");
+const resultTip = document.getElementById("result-tip");
