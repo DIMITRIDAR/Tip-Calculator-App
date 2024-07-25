ff68568e991bbd2211b92453a33028b0b4f20e12
DIMITRIDAR <daraseliadimitri@gmail.com>
2024-07-26 02:07:07 +0400

    Add event listener to the customTipInput, peopleInput, tipInputs and billInput

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
 const billInput = document.getElementById("bill");
-const tipInputs = Array.from(document.getElementsByClassName("tip;"));
+const tipInputs = Array.from(document.getElementsByClassName("tip-hover"));
 const peopleInput = document.getElementById("people");
 const resultTip = document.getElementById("result-tip");
 const resultTotal = document.getElementById("result-total");
 const resetButton = document.getElementById("reset");
 const customTipInput = document.getElementById("custom-tip");
+
+let billValue = 0;
+let tipValue = 0;
+let peopleValue = 0;
+billInput.addEventListener("input", (event) => {
+  billValue = Number(event.target.value);
+  calculation();
+});
+tipInputs.forEach((button) =>
+  button.addEventListener("click", (event) => {
+    tipValue = parseInt(event.target.innerText);
+    calculation();
+  })
+);
+
+peopleInput.addEventListener("input", (event) => {
+  peopleValue = Number(event.target.value);
+  calculation();
+});
+
+customTipInput.addEventListener("input", (event) => {
+  tipValue = parseIn(event.target.value);
+  calculation();
+});
